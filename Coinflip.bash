8d14a59a0e5d5361c7299a4bb26e2120bb68a590
shinjox27 <36406529+shinjox27@users.noreply.github.com>
2018-08-21 16:02:51 -0700

    Create Coinflip.bash

---
+++ Coinflip.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+#CoinFlip 1.0
+#Lucino Kumaru 8/21/2018
+scorefile="CFscorefile_bash"
+guess=$user_choice
+printf "Choose (h)eads or (t)ails: "
+read user_choice
+# Make sure user chooses between heads or tails
+if [ $user_choice != h ] && [ $user_choice != t ]; then
+echo "Invalid choice. Defaulting to (h)eads."
+user_choice=h
+fi
+#Value of 1 is heads, 2 is tails
+computer_choice=$((RANDOM % 2 + 1))
+if [ $computer_choice -eq 1 ]; then
+echo "Computer chose heads."
+else
+echo "Computer chose tails."
+fi
+if [ $computer_choice -eq 1 ] && [ $user_choice = h ]; then
+# Correct
+echo "You win!"
+elif [ $computer_choice -eq 1 ] && [ $user_choice = t     | ]; then
+# Incorrect
+echo "You lose!"
+exit
+elif [ $computer_choice -eq 2 ] && [ $user_choice = t ]; then
+# Correct
+echo "You win!"
+else
+# Incorrect
+echo "You lose!"
+exit
+fi
+# Save High Score
+read -p "Please enter your name: " name
+echo $name $user_choice >> $scorefile
+# print High Score
+echo -e "\nPrevious high scores,"
+cat $scorefile
