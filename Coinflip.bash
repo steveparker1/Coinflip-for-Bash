f8eca7bccdd69de2a24368e5498266edd9110fca
shinjox27 <36406529+shinjox27@users.noreply.github.com>
2018-08-24 00:00:45 -0700

    Update Coinflip.bash
    Coinflip 2.0 added ASCII art!

--- Coinflip.bash
+++ Coinflip.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-#CoinFlip 1.5
-#Lucino Kumaru 8/21/2018
+#CoinFlip 2.0
+#Lucino Kumaru 8/21/2018 shinjox27@gmail.com
 scorefile="CFscorefile_bash"
+cat ASCII/CF_opening.txt
 printf "Choose (h)eads or (t)ails: "
 read user_choice
 # Make sure user chooses between heads or tails
@@ -12,30 +13,37 @@ fi
 #Value of 1 is heads, 2 is tails
 computer_choice=$((RANDOM % 2 + 1))
 if [ $computer_choice -eq 1 ]; then
+notify-send "Heads!"
 echo "Computer chose heads."
 else
+notify-send "Tails!"
 echo "Computer chose tails."
 fi
 if [ $computer_choice -eq 1 ] && [ $user_choice = h ]; then
-# Correct & Desktop Notifaction
+# Correct & Desktop Notifaction & ASCII
+cat ASCII/CF_correct.txt
 echo "You win!"
 notify-send "your getting Clairvoyant :)"
 elif [ $computer_choice -eq 1 ] && [ $user_choice = t ]; then
-# Incorrect & Desktop Notifaction
+# Incorrect & Desktop Notifaction & ASCII
+cat ASCII/CF_incorrect.txt
 echo "You lose!"
 notify-send "Concentrate Harder! :("
 exit
 elif [ $computer_choice -eq 2 ] && [ $user_choice = t ]; then
-# Correct & Desktop Notifaction
+# Correct & Desktop Notifaction & ASCII
+cat ASCII/CF_correct.txt
 echo "You win!"
 notify-send "your getting Clairvoyant :)"
 else
-# Incorrect & Desktop Notifaction
+# Incorrect & Desktop Notifaction & ASCII
+cat ASCII/CF_incorrect.txt
 echo "You lose!"
 notify-send "Concentrate Harder! :("
 exit
 fi
-# Save High Score
+# Save High Score & ASCII
+cat ASCII/CF_scorefile.txt
 read -p "Please enter your name: " name
 echo $name $user_choice >> $scorefile
 # print High Score
